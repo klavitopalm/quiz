ffce7284018572a9b8dd8019ca1055b070fca265
klavitopalm <joshua71@gmx.net>
2015-09-09 23:14:21 +0200

    modularization is actually working now

--- server/quizCommunication.js
+++ server/quizCommunication.js
@@ -1,55 +1,62 @@
-io = require('./server');
-
-io.sockets.on('connection', function(socket) {
-
-//    setTimeout(function() {
-    socket.emit('setNewQuestion', {questionId: 'id1', questionNumber: 1,
-			questionText: 'Which hero has hook shot?',
-			answers : ['Spirit Breaker', 'Clockwerk', 'Gyrocopter', 'Storm Spirit'
-		]});
-
-		console.log('first question sent...');
-  //  }, 2000);
-
-    socket.on('questionAnswered', function(payload) {
-		console.log('questionId: ' + payload.questionId + ', answer: '+ payload.givenAnswer);
-        //socket.broadcast.emit('ready');
-
-				socket.emit('setNewQuestion', {questionId: 'id2', questionNumber: 2,
-				questionText: 'Does Stengus know how to last hit?',
-				answers : ['No, biggest noob, eva!!!1', 'Know, yes. Does he want to? No!', 'Who the fuck is Stengus?'
-				]});
-
-				console.log('second question sent...');
 
 
-    });
+module.exports = function(server) {
+  var io = require('socket.io').listen(server);
 
 
-	socket.on('name', function(payload) {
-		var fingerprint = payload.fingerprint;
-		var name = payload.name;
+  io.sockets.on('connection', function(socket) {
 
-		mongoClient.connect(url, function(err, db) {
-			assert.equal(null, err);
-			var collection = db.collection('identification');
-			console.log('fingerprint: ' + fingerprint +', name: '+ name);
+  //    setTimeout(function() {
+      socket.emit('setNewQuestion', {questionId: 'id1', questionNumber: 1,
+  			questionText: 'Which hero has hook shot?',
+  			answers : ['Spirit Breaker', 'Clockwerk', 'Gyrocopter', 'Storm Spirit'
+  		]});
 
-			collection.insert({fingerprint: fingerprint, name: name}, function (err, result)
-			{
-			console.log('testoutput 2');
-				if(err) {
-				console.log('testoutput 3');
-					console.log(err);
-				}
-				else {
-				console.log('testoutput 4');
-					console.log('Inserted document');
-				}
-				db.close();
-			});
+  		console.log('first question sent...');
+    //  }, 2000);
+
+      socket.on('questionAnswered', function(payload) {
+  		console.log('questionId: ' + payload.questionId + ', answer: '+ payload.givenAnswer);
+          //socket.broadcast.emit('ready');
+
+  				socket.emit('setNewQuestion', {questionId: 'id2', questionNumber: 2,
+  				questionText: 'Does Stengus know how to last hit?',
+  				answers : ['No, biggest noob, eva!!!1', 'Know, yes. Does he want to? No!', 'Who the fuck is Stengus?'
+  				]});
+
+  				console.log('second question sent...');
 
 
-		});
-	});
-});
+      });
+
+
+  	socket.on('name', function(payload) {
+  		var fingerprint = payload.fingerprint;
+  		var name = payload.name;
+
+  		mongoClient.connect(url, function(err, db) {
+  			assert.equal(null, err);
+  			var collection = db.collection('identification');
+  			console.log('fingerprint: ' + fingerprint +', name: '+ name);
+
+  			collection.insert({fingerprint: fingerprint, name: name}, function (err, result)
+  			{
+  			console.log('testoutput 2');
+  				if(err) {
+  				console.log('testoutput 3');
+  					console.log(err);
+  				}
+  				else {
+  				console.log('testoutput 4');
+  					console.log('Inserted document');
+  				}
+  				db.close();
+  			});
+
+
+  		});
+  	});
+  });
+
+  return io;
+}
--- server/server.js
+++ server/server.js
@@ -24,63 +24,8 @@ app.use(express.static(__dirname + '/../public', { maxAge: oneDay }));
 
 var port = process.env.PORT || 3000;
 
-var io = socket.listen(app.listen(port));
+var io = require('./quizCommunication')(app.listen(port));
 
-console.log('Example app listening at http://localhost:%s', port);
+//socket.listen(app.listen(port));
 
-
-module.exports.io = io; 
-
-io.sockets.on('connection', function(socket) {
-
-    setTimeout(function() {
-        socket.emit('setNewQuestion', {questionId: 'id1', questionNumber: 1,
-				questionText: 'Which hero has hook shot?',
-				answers : ['Spirit Breaker', 'Clockwerk', 'Gyrocopter', 'Storm Spirit'
-			]});
-
-				console.log('first question sent...');
-    }, 2000);
-
-    socket.on('questionAnswered', function(payload) {
-		console.log('questionId: ' + payload.questionId + ', answer: '+ payload.givenAnswer);
-        //socket.broadcast.emit('ready');
-
-				socket.emit('setNewQuestion', {questionId: 'id2', questionNumber: 2,
-				questionText: 'Does Stengus know how to last hit?',
-				answers : ['No, biggest noob, eva!!!1', 'Know, yes. Does he want to? No!', 'Who the fuck is Stengus?'
-				]});
-
-				console.log('second question sent...');
-
-
-    });
-
-
-	socket.on('name', function(payload) {
-		var fingerprint = payload.fingerprint;
-		var name = payload.name;
-
-		mongoClient.connect(url, function(err, db) {
-			assert.equal(null, err);
-			var collection = db.collection('identification');
-			console.log('fingerprint: ' + fingerprint +', name: '+ name);
-
-			collection.insert({fingerprint: fingerprint, name: name}, function (err, result)
-			{
-			console.log('testoutput 2');
-				if(err) {
-				console.log('testoutput 3');
-					console.log(err);
-				}
-				else {
-				console.log('testoutput 4');
-					console.log('Inserted document');
-				}
-				db.close();
-			});
-
-
-		});
-	});
-});
+console.log('App listening at http://localhost:%s', port);
