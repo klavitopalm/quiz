b89ba374ca503e6947fd91efa44a230338717424
klavitopalm <joshua71@gmx.net>
2015-09-09 19:55:23 +0200

    server-side socket.io code is now in a separate file

--- server/quizCommunication.js
+++ server/quizCommunication.js
@@ -1,46 +1,15 @@
-var express = require('express');
-var compress = require('compression');
-var socket = require('socket.io');
-var mongoClient = require('mongodb').MongoClient, assert = require('assert');
-var url = 'mongodb://localhost:27017/test';
-
-var db;
-
-mongoClient.connect(url, function(err, db) {
-	assert.equal(null, err);
-	console.log("connected to db server");
-
-	db.close();
-});
-
-var app = express();
-
-app.use(compress());
-
-var oneDay = 86400000;
-oneDay = 0;
-
-app.use(express.static(__dirname + '/../public', { maxAge: oneDay }));
-
-var port = process.env.PORT || 3000;
-
-var io = socket.listen(app.listen(port));
-
-console.log('Example app listening at http://localhost:%s', port);
-
-
-module.exports.io = io; 
+io = require('./server');
 
 io.sockets.on('connection', function(socket) {
 
-    setTimeout(function() {
-        socket.emit('setNewQuestion', {questionId: 'id1', questionNumber: 1,
-				questionText: 'Which hero has hook shot?',
-				answers : ['Spirit Breaker', 'Clockwerk', 'Gyrocopter', 'Storm Spirit'
-			]});
+//    setTimeout(function() {
+    socket.emit('setNewQuestion', {questionId: 'id1', questionNumber: 1,
+			questionText: 'Which hero has hook shot?',
+			answers : ['Spirit Breaker', 'Clockwerk', 'Gyrocopter', 'Storm Spirit'
+		]});
 
-				console.log('first question sent...');
-    }, 2000);
+		console.log('first question sent...');
+  //  }, 2000);
 
     socket.on('questionAnswered', function(payload) {
 		console.log('questionId: ' + payload.questionId + ', answer: '+ payload.givenAnswer);
